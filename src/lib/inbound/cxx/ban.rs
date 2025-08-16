555d5a646ef7c2795c220b37abf55e559a7ed83f
max <wak@vanling.net>
2025-08-16 21:28:25 +0200

    Create basic ban implementation

--- src/lib/inbound/cxx/ban.rs
+++ src/lib/inbound/cxx/ban.rs
@@ -1,22 +1,24 @@
+use chrono::{Duration, Utc};
+
 use crate::domain::mumble::models::ban::Ban;
 
 #[allow(unsafe_op_in_unsafe_fn)]
-#[cxx::bridge(namespace = "com::maxer137::libmumble")]
+#[cxx::bridge(namespace = "mumblers::ban")]
 pub mod ffi {
     extern "Rust" {
-        fn lib_cxxbridge_bool(some: bool) -> bool;
+        type Ban;
 
-        fn greet(name: &str) -> String;
+        fn new_ban(network: &str, name: &str, hash: &str) -> Box<Ban>;
     }
 }
 
-pub fn lib_cxxbridge_bool(some: bool) -> bool {
-    if some {
-        return false;
-    }
-    true
-}
-
-fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
+fn new_ban(network: &str, name: &str, hash: &str) -> Box<Ban> {
+    Box::new(Ban {
+        network: network.parse().unwrap(),
+        username: name.to_string(),
+        hash: hash.to_string(),
+        reason: "".to_string(),
+        start: Utc::now(),
+        duration: Some(Duration::zero()),
+    })
 }
